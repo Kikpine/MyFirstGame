588b2efaa2b70d9b95108027bd3856b1245fef46
Arseniy Parakhin <senya.paraxin.04@mail.ru>
2024-11-22 12:31:47 +0300

    Change Y phisics to AddForce
    AddForce better for this project

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,42 +4,51 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
 
-    private float speedX;
-    private float speedY;
-    private float propellerRpm;
-    private float propellerMAXRpm;
-    private float rosh;
-    public float GRAVITY = 1;
-    public float mass;
+    private float engineSpeed = 1000;
 
-    void Awake()
+    //public float speedX;
+    //public float speedY;
+    //public float propellerMAXRpm;
+    //public float rosh;
+    //public float GRAVITY = 1;
+    //public float mass;
+
+    void Start()
     {
         body = GetComponent<Rigidbody2D>();
 
-        body.gravityScale = GRAVITY;
-        body.velocity = Vector2.zero;
-        body.mass = 7.2f;
+        //body.gravityScale = GRAVITY;
+        //body.velocity = Vector2.zero;
+        //body.mass = 7.2f;
 
-        //propellerRpm  = 0f;
-        //propellerMAXRpm = 192f;
-        rosh = 0f; // пнь Б %
+        ////engineSpeed  = 0f;
+        ////propellerMAXRpm = 192f;
+        //rosh = 0f; // пнь Б %
 
-        speedX = 5f;
-        speedY = 50f;
+        //speedX = 5f;
+        //speedY = 50f;
 
-}
-void Update()
+    }
+    void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speedX, (rosh / 100f * speedY / body.mass) - (body.gravityScale * 5));
+        //body.velocity = new Vector2(Input.GetAxis("Horizontal") * speedX, (rosh / 100f * speedY / body.mass) - (body.gravityScale * 5));
+
         
-        if (Input.GetKey(KeyCode.UpArrow) && rosh <= 100f)
+
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-            rosh += 0.1f;
+            body.AddForce(transform.up * engineSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.DownArrow) && rosh > -0.1f)
-        {
-            rosh -= 0.1f;
-        }
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    body.AddForce(transform.up * rosh, ForceMode2D.Impulse);
+        //}
+
+        //if (Input.GetKey(KeyCode.DownArrow) && rosh > -0.1f)
+        //{
+        //    rosh -= 0.1f;
+        //}
     }
 }
 
